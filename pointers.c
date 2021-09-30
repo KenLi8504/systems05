7234e08657343d280d538d4ae2d97e655ff3e3f4
KenLi8504 <kli22@stuy.edu>
2021-09-30 03:27:08 -0400

    I think I'm done

--- pointers.c
+++ pointers.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main(){
-  //part 1
+  printf("Part 1 \n");
   char character = 'a';
   int integer = 10;
   long longnumber = 2147483900;
 
-  //part 2
+  printf("Part 2 \n");
   //Observation: Addresses are close together (apart by a few bytes)
   printf("The location of the character in hex is %p\n",&character);
   printf("The location of the integer in hex is %p\n", &integer);
@@ -15,17 +15,17 @@ int main(){
   printf("The location of the integer in decimal is %lu\n", &integer);
   printf("The location of the long in decimal is %lu\n", &longnumber);
 
-  //part 3
+  printf("Part 3 \n");
   char *charPoint = &character;
   int *intPoint = &integer;
   long *longPoint = &longnumber;
 
-  //part 4
+  printf("Part 4 \n");
   printf("The character pointer is %p\n",charPoint);
   printf("The integer pointer is %p\n", intPoint);
   printf("The long pointer is %p\n", longPoint);
 
-  //part 5
+  printf("Part 6 \n");
   character = *charPoint + 1;
   integer = *intPoint + 1;
   longnumber = *longPoint + 1;
@@ -34,6 +34,54 @@ int main(){
   printf("The new integer is %d\n",integer);
   printf("The new longnumber is %ld\n",longnumber);
 
-  //part 6
+  printf("Part 6 \n");
+  unsigned int noSigns = 62;
+  int *intPointerUS = &noSigns;
+  char *charPointerUS = &noSigns;
+
+  printf("Part 7 \n");
+  printf("The value of the integer pointer is %p and it points to %lu \n",intPointerUS,noSigns);
+  printf("The value of the char pointer is %p and it points to %c \n",charPointerUS,noSigns);
+
+  printf("Part 8 \n");
+  printf("The unsigned int as a decimal/unsigned integer is %u \n", noSigns);
+  printf("The unsigned int as a hexadecimal is %x \n", noSigns);
+
+  printf("Part 9 \n");
+  printf("The unsigned integer in decimal is %hhx %hhx %hhx %hhx \n"
+  ,*(charPointerUS),*(charPointerUS+1),*(charPointerUS+2),*(charPointerUS+3));
+  printf("The unsigned integer in hex is %hhu %hhu %hhu %hhu \n"
+  ,*charPointerUS,*(charPointerUS+1),*(charPointerUS+2),*(charPointerUS+3));
+
+
+  printf("Part 10 \n");
+    *(charPointerUS) = *(charPointerUS) + 1;
+    printf("Adding 1 to the first byte will give in hex is %x and in decimal is %u \n",*(charPointerUS),*(charPointerUS) );
+    *(charPointerUS+1) = *(charPointerUS+1) + 1;
+    printf("Adding 1 to the second byte will give in hex is %x and in decimal is %u \n",*(charPointerUS+1),*(charPointerUS+1) );
+    *(charPointerUS+2) = *(charPointerUS+2) + 1;
+    printf("Adding 1 to the third byte will give in hex is %x and in decimal is %u \n",*(charPointerUS+2),*(charPointerUS+2) );
+    *(charPointerUS+3) = *(charPointerUS+3) + 1;
+    printf("Adding 1 to the fourth byte will give in hex is %x and in decimal is %u \n",*(charPointerUS+3),*(charPointerUS+3) );
+
+    printf("The new unsigned integer in decimal is %hhx %hhx %hhx %hhx \n"
+    ,*(charPointerUS),*(charPointerUS+1),*(charPointerUS+2),*(charPointerUS+3));
+    printf("The new unsigned integer in hex is %hhu %hhu %hhu %hhu \n"
+    ,*charPointerUS,*(charPointerUS+1),*(charPointerUS+2),*(charPointerUS+3));
+
+  printf("Part 11 \n");
+  *(charPointerUS) = *(charPointerUS) + 16;
+  printf("Adding 16 to the first byte will give in hex is %x and in decimal is %u \n",*(charPointerUS),*(charPointerUS) );
+  *(charPointerUS+1) = *(charPointerUS+1) + 16;
+  printf("Adding 16 to the second byte will give in hex is %x and in decimal is %u \n",*(charPointerUS+1),*(charPointerUS+1) );
+  *(charPointerUS+2) = *(charPointerUS+2) + 16;
+  printf("Adding 16 to the third byte will give in hex is %x and in decimal is %u \n",*(charPointerUS+2),*(charPointerUS+2) );
+  *(charPointerUS+3) = *(charPointerUS+3) + 16;
+  printf("Adding 16 to the fourth byte will give in hex is %x and in decimal is %u \n",*(charPointerUS+3),*(charPointerUS+3) );
+
+  printf("The new unsigned integer in decimal is %hhx %hhx %hhx %hhx \n"
+  ,*(charPointerUS),*(charPointerUS+1),*(charPointerUS+2),*(charPointerUS+3));
+  printf("The new unsigned integer in hex is %hhu %hhu %hhu %hhu \n"
+  ,*charPointerUS,*(charPointerUS+1),*(charPointerUS+2),*(charPointerUS+3));
   return 0;
 }
